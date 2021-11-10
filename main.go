9943a6342be4a68e501dc49b8f9982b59906df5d
YTMartian <1773741250@qq.com>
2021-11-10 23:45:31 +0800

    Update main.go

--- main.go
+++ main.go
@@ -5,7 +5,7 @@ import (
 // "database/sql"
 "fmt"
 "os"
-"github.com/gofiber/fiber/v2"
+// "github.com/gofiber/fiber/v2"
 // _ "github.com/go-sql-driver/mysql"
 )
 func getEnv(env string, defaultVal string) (key string) {
@@ -32,9 +32,9 @@ func main() {
 // 		panic(err)
 // 	}
 	// http server
-	app := fiber.New()
-	app.Get("/", func(c *fiber.Ctx) error {
-		return c.SendString("Hello, World!!!")
-	})
-	panic(app.Listen(":8080"))
+// 	app := fiber.New()
+// 	app.Get("/", func(c *fiber.Ctx) error {
+// 		return c.SendString("Hello, World!!!")
+// 	})
+// 	panic(app.Listen(":8080"))
 }
