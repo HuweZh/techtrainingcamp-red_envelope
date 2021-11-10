a876791be7429ce0642f028b8a1cc24220dfbfa7
YTMartian <1773741250@qq.com>
2021-11-10 22:20:32 +0800

    Update main.go

--- main.go
+++ main.go
@@ -6,7 +6,7 @@ import (
 "fmt"
 "os"
 "github.com/gofiber/fiber/v2"
-_ "github.com/go-sql-driver/mysql"
+// _ "github.com/go-sql-driver/mysql"
 )
 func getEnv(env string, defaultVal string) (key string) {
 	if key = os.Getenv(env); key == "" {
@@ -23,13 +23,13 @@ func main() {
 	port := getEnv("MYSQL_SERVICE_PORT", "3306")
 	password := getEnv("MYSQL_ROOT_PASSWORD", "root")
 	dsn := fmt.Sprintf("root:%s@tcp(%s:%s)/red_envelope", password, host, port)
-	db, err := sql.Open("mysql", dsn)
-	if err != nil {
-		panic(err)
-	}
-	if err = db.Ping(); err != nil {
-		panic(err)
-	}
+// 	db, err := sql.Open("mysql", dsn)
+// 	if err != nil {
+// 		panic(err)
+// 	}
+// 	if err = db.Ping(); err != nil {
+// 		panic(err)
+// 	}
 	// http server
 	app := fiber.New()
 	app.Get("/", func(c *fiber.Ctx) error {
